ba937b95e9df632566266dbaea3807f5f050ffe1
Cupmouse <Cupmouse@users.noreply.github.com>
2015-07-08 21:56:38 +0900

    GSImageEffectとRGBImageEffectがImageEffectを継承していなかったミスを修正

--- src/main/java/net/spicesoftware/api/image/effect/RGBImageEffect.java
+++ src/main/java/net/spicesoftware/api/image/effect/RGBImageEffect.java
@@ -3,7 +3,7 @@ package net.spicesoftware.api.image.effect;
 /**
  * @since 2015/05/09
  */
-public interface RGBImageEffect {
+public interface RGBImageEffect extends ImageEffect {
 
     int[] applyRGB(int width, int height, int[] data);
 }
