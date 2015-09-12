c6c0d98c149c9ad4392c902fe1b72278c225fa1a
Cupmouse <Cupmouse@users.noreply.github.com>
2015-09-12 09:42:34 +0900

    FrameRangedの変更 FrameRangedにrangeメソッドを追加 javadoc修正と追加

--- src/main/java/net/spicesoftware/api/util/time/FrameRanged.java
+++ src/main/java/net/spicesoftware/api/util/time/FrameRanged.java
@@ -11,7 +11,8 @@ import static net.spicesoftware.api.util.Validate.nullNot;
 import static net.spicesoftware.api.util.Validate.smaller;
 
 /**
- * フレーム間隔とその間隔が適用されるオブジェクトを保持するクラスです。
+ * フレーム間隔とそれが適用されるオブジェクトのインスタンスを保持するクラスです。
+ * イミュータブルです。
  *
  * @since 2014/10/06
  */
@@ -21,7 +22,16 @@ public final class FrameRanged<T extends DeepCopyable> implements ShallowCopyabl
     public final int start;
     public final int end;
 
-    public FrameRanged(T ranged, @Min(0) int start, @Min(0) int end) throws IllegalArgumentException {
+    /**
+     * 範囲と範囲が適用されるインスタンス
+     *
+     * @param ranged 範囲が適用されるインスタンス
+     * @param start 範囲の開始
+     * @param end 範囲の終了
+     * @throws NullPointerException rangedにnullが指定された時
+     * @throws IllegalArgumentException startがendより大きい時
+     */
+    public FrameRanged(T ranged, @Min(0) int start, @Min(0) int end) throws NullPointerException, IllegalArgumentException {
         nullNot(ranged);
         smaller(start, end);
         this.ranged = ranged;
@@ -29,18 +39,42 @@ public final class FrameRanged<T extends DeepCopyable> implements ShallowCopyabl
         this.end = end;
     }
 
+    /**
+     * 新しい範囲を指定して同じインスタンスを持つ{@code FrameRanged}を作成します。
+     * 
+     * @param start 新しい範囲の開始
+     * @param end 新しい範囲の終了
+     * @return 新しい範囲で同じオブジェクトのインスタンスを持つ{@code FrameRanged}
+     */
+    public FrameRanged<T> range(@Min(0) int start, @Min(0) int end) throws IllegalArgumentException {
+        return new FrameRanged<>(ranged, start, end);
+    }
+
+    /**
+     * 指定された範囲同士が衝突している（範囲を共有している部分がある）かどうかを検証し、真偽値で返します。
+     *
+     * @param ranged1 衝突の検証をする範囲
+     * @param ranged2 衝突の検証をする範囲
+     * @return 指定された範囲同士が衝突している場合trueを、していない場合false
+     */
     public static boolean doesCollision(FrameRanged ranged1, FrameRanged ranged2) {
         return ranged1.start < ranged2.start ? ranged1.end > ranged2.start : ranged2.end > ranged1.start;
     }
 
-    public boolean isCollisionWith(FrameRanged another) {
-        nullNot(another);
-        return doesCollision(this, another);
+    /**
+     * 指定された範囲とこの範囲が衝突している（範囲を共有している部分がある）かどうかを検証し、真偽値で返します。
+     *
+     * @param frameRanged 衝突の検証をする範囲
+     * @return 指定された範囲と衝突している場合trueを、していない場合false
+     */
+    public boolean isCollisionWith(FrameRanged frameRanged) {
+        nullNot(frameRanged);
+        return doesCollision(this, frameRanged);
     }
 
     /**
      * 範囲をシャローコピーします。
-     * rangedはコピーされず、全く同じインスタンスが格納されます。
+     * rangedはコピーされず、全く同じインスタンスが格納された新しい{@link FrameRanged}のインスタンスが返されます。
      *
      * @return 範囲のシャローコピー
      */
