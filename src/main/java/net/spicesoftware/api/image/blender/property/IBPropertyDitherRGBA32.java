4310fc0bf08f708faaeabf6aca7d83ece6471f87
Cupmouse <Cupmouse@users.noreply.github.com>
2015-09-13 15:48:34 +0900

    IBPropertyDitherRGBA32がIBPropertyOpacityを継承するようになった

--- src/main/java/net/spicesoftware/api/image/blender/property/IBPropertyDitherRGBA32.java
+++ src/main/java/net/spicesoftware/api/image/blender/property/IBPropertyDitherRGBA32.java
@@ -3,7 +3,7 @@ package net.spicesoftware.api.image.blender.property;
 /**
  * @since 2015/09/07
  */
-public interface IBPropertyDitherRGBA32 extends ImageBlenderProperty {
+public interface IBPropertyDitherRGBA32 extends IBPropertyOpacity {
 
     int getSeed();
 }
