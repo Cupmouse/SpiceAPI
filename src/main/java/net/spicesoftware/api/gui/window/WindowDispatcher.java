982fb3f66fdc33ebf21e447a29a2b92798442253
Cupmouse <Cupmouse@users.noreply.github.com>
2015-12-30 18:22:11 +0900

    WindowDispatcherの修正

--- src/main/java/net/spicesoftware/api/gui/window/WindowDispatcher.java
+++ src/main/java/net/spicesoftware/api/gui/window/WindowDispatcher.java
@@ -1,6 +1,7 @@
 package net.spicesoftware.api.gui.window;
 
 import net.spicesoftware.api.util.AlreadyRegisteredException;
+import net.spicesoftware.api.util.Pair;
 
 import java.util.Optional;
 import java.util.Set;
@@ -9,31 +10,31 @@ import java.util.function.Supplier;
 /**
  * @since 2015/10/28
  */
-public interface WindowDispatcher<WS extends WindowSystem> {
+public interface WindowDispatcher<WS extends WindowSystem, WC extends Window<WS>> {
 
-    <W extends Window<WS>> W createNewWindow(Class<W> clazz);
+    <W extends WC> Pair<W, WindowStateImmutable> createNewWindow(Class<W> clazz);
 
-    void registerWindowSupplier(String id, Supplier<Window<WS>> windowSupplier, WindowStateImmutable defaultState) throws AlreadyRegisteredException;
+    void registerWindowSupplier(String id, Supplier<? extends WC> windowSupplier, WindowStateImmutable defaultState) throws AlreadyRegisteredException;
 
-    Set<WindowSession<WS, ? extends Window<WS>>> getDispensed();
+    Set<? extends WindowSession<WS, ? extends Window<WS>>> getDispensed();
 
-    <W extends Window<WS>> Set<WindowSession<WS, W>> getDispensed(Class<W> clazz);
+    <W extends WC> Set<? extends WindowSession<WS, W>> getDispensed(Class<W> clazz);
 
     /**
      * 指定された{@link Window}をディスペンスします。
      * {@link WindowState}は指定された{@link Window}のデフォルトが適用されます。
      *
      * @param window ディスペンスする{@link Window}
-     * @return ディスペンスされた{@link Window}
+     * @return ディスペンスされた{@link WindowSession}
      */
-    <W extends Window<WS>> Optional<WindowSession<WS, W>> dispense(W window);
+    <W extends WC> WindowSession<WS, W> dispense(W window);
 
     /**
      * 指定された{@link Window}を指定された{@link WindowState}でディスペンスします。
      *
      * @param window      ディスペンスする{@link Window}
      * @param windowState ディスペンスする{@link Window}の{@link WindowState}
-     * @return ディスペンスされた{@link Window}
+     * @return ディスペンスされた{@link WindowSession}
      */
-    <W extends Window<WS>> Optional<WindowSession<WS, W>> dispense(Window<WS> window, WindowStateMutable windowState);
+    <W extends WC> WindowSession<WS, W> dispense(W window, WindowState windowState);
 }
